4bc4231baa34b4d2e89a0b5c27eec5ddaa6269a6
Addy <zaristus@gmail.com>
2020-07-16 13:19:00 -0700

    sign up

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -6,7 +6,7 @@ import 'react-toastify/dist/ReactToastify.css';
 
 
 
-const SignUpContainer = props => {
+const SignUpContainer = async (props) => {
     const [firstName, setFirstName] = useState('');
     const [lastName, setLastName] = useState('');
     const [email, setEmail] = useState('');
@@ -19,7 +19,7 @@ const SignUpContainer = props => {
     const [favPet, setFavPet] = useState('dog');
     const [spontaneous, setSpontaneous] = useState(true);
     const [intoTech, setIntoTech] = useState(true);
-    const [introvert, setintrovert] = useState(true);
+    const [introvert, setIntrovert] = useState(true);
     
     const NameInput = props => {  
         const firstNameChange = e => {
@@ -123,14 +123,106 @@ const SignUpContainer = props => {
         }
 
         const handleClick = (e) =>{
-            setNextComponent()
+            setNextComponent(<PetInput />)
         }
 
         return(
             <div className='signUp__question-div'>
                 <h2 className='signUp__question-header'>Do you like puns?</h2>
-                <select>
-                    <option value={true}></option>
+                <select onChange={punChange}>
+                    <option value={true}>Yes</option>
+                    <option value={false}>No, I'm not punny enough.</option>
+                </select>
+                
+                <button onClick={handleClick}>Continue</button>
+            </div>
+        )
+    }
+
+    const PetInput = props => {
+        const petChange = e => {
+            setFavPet(e.target.value)
+        }
+
+        const handleClick = (e) =>{
+            setNextComponent(<SpontanietyCheck />)
+        }
+
+        return(
+            <div className='signUp__question-div'>
+                <h2 className='signUp__question-header'>What's your ideal pet?</h2>
+                <select onChange={petChange}>
+                    <option value='dog'>Dogs</option>
+                    <option value='cat'>Cats</option>
+                    <option value='lizard'>Lizards</option>
+                    <option value='spider'>Spiders</option>
+                    <option value='fish'>Fish</option>
+                </select>
+                
+                <button onClick={handleClick}>Continue</button>
+            </div>
+        )
+    }
+
+    const SpontanietyCheck = props => {
+        const spontaneousChange = e => {
+            setSpontaneous(e.target.value)
+        }
+
+        const handleClick = (e) =>{
+            setNextComponent(<IntrovertCheck />)
+        }
+
+        return(
+            <div className='signUp__question-div'>
+                <h2 className='signUp__question-header'>Do you tend towards planning, or spontaneity?</h2>
+                <select onChange={spontaneousChange}>
+                    <option value={true}>Spontaneity</option>
+                    <option value={false}>Pre-planning</option>
+                </select>
+                
+                <button onClick={handleClick}>Continue</button>
+            </div>
+        )
+    }
+
+    const IntrovertCheck = props => {
+        const introvertChange = e => {
+            setIntrovert(e.target.value)
+        }
+
+        const handleClick = (e) =>{
+            setNextComponent(<IntoTechCheck />)
+        }
+
+        return(
+            <div className='signUp__question-div'>
+                <h2 className='signUp__question-header'>Are you more extroverted, or introverted?</h2>
+                <select onChange={introvertChange}>
+                    <option value={true}>Introverted</option>
+                    <option value={false}>Extroverted</option>
+                </select>
+                
+                <button onClick={handleClick}>Continue</button>
+            </div>
+        )
+    }
+
+    const IntoTechCheck = props => {
+        const intoTechChange = e => {
+            setIntoTech(e.target.value)
+        }
+
+        const handleClick = (e) =>{
+            setNextComponent(<EmailPass />)
+        }
+
+        return(
+            <div className='signUp__question-div'>
+                <h2 className='signUp__question-header'>Are you more extroverted, or introverted?</h2>
+                <select onChange={intoTechChange}>
+                    <option value={true}>Introverted</option>
+                    <option value={false}>Extroverted</option>
                 </select>
                 
                 <button onClick={handleClick}>Continue</button>
@@ -139,8 +231,38 @@ const SignUpContainer = props => {
     }
 
 
+    const EmailPass = props => {
+        const emailChange = e => {
+            setEmail(e.target.value)
+        }
+
+        const passChange = e => {
+            setPassword(e.target.value)
+        }
+
+        const handleClick = async (e) =>{
+            if(firstName && lastName && email && password){
+            await props.createUser(firstName, lastName, email, password, gender, genderPref, imgUrl, spectrum, likesPuns, favPet, spontaneous, intoTech, introvert)
+            }
+            else toast('Please fill out all fields!')
+        }
+
+        return(
+            <div className='signUp__question-div'>
+                <h2 className='signUp__question-header'>Are you more extroverted, or introverted?</h2>
+                <label>Email: <input type='email' onChange={emailChange}></input></label>
+                <label>Password: <input type='password' onChange={passChange}></input></label>
+                <button onClick={handleClick}>Sign up!</button>
+            </div>
+        )
+    }
+
+
     return(
-        nextComponent
+        <>
+        {nextComponent}
+        <ToastContainer />
+        </>
     )
 }
 
