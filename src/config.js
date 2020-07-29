64b637c53fa0c4c99d3fe7672f7285c9067d7915
aderyn1121 <zaristus@gmail.com>
2020-07-29 10:33:05 -0700

    more heroku prep

--- src/config.js
+++ src/config.js
@@ -1,1 +1,1 @@
-export const baseUrl = process.env.NODE_ENV === 'development' ? 'http://localhost:5000' : process.env.BASE_URL;
+export const baseUrl = process.env.BASE_URL || 'http://localhost:5000';
